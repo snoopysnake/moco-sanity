df8b21dda628b5c81577fc97c902d02eb01a8984
snoopysnake <chiang4@gmail.com>
2024-07-29 02:17:00 -0400

    remove file

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,7 +1,7 @@
 import {formType} from './formType'
 import {heroType} from './heroType'
 import {imageGalleryType} from './imageGalleryType'
-import {postType} from './postType'
+// import {postType} from './postType'
 import {textWithIllustrationType} from './textWithIllustrationType'
 import {videoType} from './videoType'
 import {ratesType} from './ratesType'
@@ -12,7 +12,7 @@ import { classType } from './classType'
 import { coachesType } from './coachesType'
 
 export const schemaTypes = [
-  postType,
+  // postType,
   heroType,
   textWithIllustrationType,
   imageGalleryType,
@@ -20,7 +20,6 @@ export const schemaTypes = [
   videoType,
   ratesType,
   membershipType,
-  // scheduleType,
   classType,
   crossfitScheduleType,
   weightliftingScheduleType,
